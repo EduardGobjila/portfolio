8f4251dd700c598c2f2cf3ffa2f532a8cfb76a62
Eduard Gobjila <edikgobjila01@gmail.com>
2022-09-23 15:02:48 +0100

    added my projects to the portfolio also finished the animation with the typing javascript

--- landing.js
+++ landing.js
@@ -1,20 +1,15 @@
-let i = 0;
-let sentece = 'You should hire me for your next success';
-let sentence = 'I can turn your usual visitors into customers';
-let speed = 60;
+// let i = 0;
+// let sentece = 'You should hire me for your next success';
+// let sentence = 'I can turn your usual visitors into customers';
+// let speed = 60;
 
-function typo() {
-   let text = document.getElementById('typing')
-  if (i < sentece.length) {
-   text.innerHTML += sentece.charAt(i);
-    i++;
-    setTimeout(typo, speed);
-  } else if (i === sentece.length) {
-    text.innerHTML = '';
-    i = 0
-    if (i < sentece.length) {
-     text.innerHTML += sentence.charAt(i)
-    }
-  }
-}
-typo()
+// function typo() {
+//    let text = document.getElementById('typing')
+//   if (i < sentece.length) {
+//    text.innerHTML += sentece.charAt(i);
+//     i++;
+//     // setTimeout(typo, speed);
+//   }
+//   setTimeout(typo, speed);
+// }
+// typo()
