5b166334d1484748caa167ed93185812d5bced1e
Eduard Gobjila <edikgobjila01@gmail.com>
2022-09-25 09:56:11 +0100

    finished the animation of the bars on a mobile phone

--- landing.js
+++ landing.js
@@ -1,15 +1,19 @@
 
-let CountButtonHomeClicks = 0
+let CountButtonHomeClicks = 1
  var links = document.getElementById("three-lines");
  links.addEventListener('click', event => {
    let link = document.getElementById('links')
    CountButtonHomeClicks += 1
    if (CountButtonHomeClicks % 2 == 0) {
     link.style.display = 'block'
-    let bars = document.getElementsByClassName('fa-bars')
-    bars = ''
+    let bars = document.getElementsByClassName('fa-bars')[0]
+    bars.classList.remove('fa-bars')
+    bars.classList.add('fa-x')
     console.log(bars)
    } else {
+    let bar = document.getElementsByClassName('fa-x')[0]
+    bar.classList.remove('fa-x')
+    bar.classList.add('fa-bars')
     link.style.display = 'none'
    }
 
