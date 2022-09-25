331139e0c583b6ee7889e92384c15222b9078d70
Eduard Gobjila <edikgobjila01@gmail.com>
2022-09-25 14:41:32 +0100

    made the about me page mobile user friendly

--- landing.js
+++ landing.js
@@ -1,20 +1,21 @@
 
 let CountButtonHomeClicks = 1
  var links = document.getElementById("three-lines");
+
  links.addEventListener('click', event => {
    let link = document.getElementById('links')
-   CountButtonHomeClicks += 1
-   if (CountButtonHomeClicks % 2 == 0) {
-    link.style.display = 'block'
-    let bars = document.getElementsByClassName('fa-bars')[0]
-    bars.classList.remove('fa-bars')
-    bars.classList.add('fa-x')
-    console.log(bars)
+    CountButtonHomeClicks += 1
+    if (CountButtonHomeClicks % 2 == 0) {
+      link.style.display = 'block'
+      let bars = document.getElementsByClassName('fa-bars')[0]
+      bars.classList.remove('fa-bars')
+      bars.classList.add('fa-x')
+
    } else {
-    let bar = document.getElementsByClassName('fa-x')[0]
-    bar.classList.remove('fa-x')
-    bar.classList.add('fa-bars')
-    link.style.display = 'none'
+      let bar = document.getElementsByClassName('fa-x')[0]
+      bar.classList.remove('fa-x')
+      bar.classList.add('fa-bars')
+      link.style.display = 'none'
    }
 
 })
