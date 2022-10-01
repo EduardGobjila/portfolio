2d01be2342c42166f59a308e8381d4d1972ae40f
Eduard Gobjila <edikgobjila01@gmail.com>
2022-10-01 01:47:38 +0100

    added comments and refactored the code a bit in landing.js

--- landing.js
+++ landing.js
@@ -1,16 +1,19 @@
 
+// Set the counter for clicks
 let CountButtonHomeClicks = 1
  var links = document.getElementById("three-lines");
 
  links.addEventListener('click', event => {
    let link = document.getElementById('links')
+  //  each click checks if it is a odd number or even
     CountButtonHomeClicks += 1
+    // if even then displaysthe navbar links and changes the nav image to x from three lines
     if (CountButtonHomeClicks % 2 == 0) {
       link.style.display = 'block'
       let bars = document.getElementsByClassName('fa-bars')[0]
       bars.classList.remove('fa-bars')
       bars.classList.add('fa-x')
-
+    // removes the nav links to a simple navbar
    } else {
       let bar = document.getElementsByClassName('fa-x')[0]
       bar.classList.remove('fa-x')
@@ -21,15 +24,17 @@ let CountButtonHomeClicks = 1
 })
 
 
+// adding a listener on scroll
 window.addEventListener('scroll', () => {
+  // checking the height of the window
   const height = document.documentElement.scrollHeight - window.innerHeight
   const scrolled = window.scrollY;
   const backToTop = document.getElementById('back')
 
-  console.log(Math.ceil(scrolled))
+  //  if scroleed is bigger than height - 100 changes the color of the button
   if(Math.ceil(scrolled) > height - 100) {
      backToTop.style.backgroundColor ='#1597BB'
-     console.log('true')
+    //  else no color
     } else {
       backToTop.style.backgroundColor = null
     }
@@ -39,12 +44,15 @@ window.addEventListener('scroll', () => {
  const faders = document.querySelectorAll('.fade-in')
  const cards = document.querySelectorAll('.cards')
 
+//  setting appear option on -300px on scrolling point
  const appearOptions = {
    threshold: 0,
    rootMargin: "0px 0px -300px 0px"
  }
 
+//  setting new observer
  const appearOnScroll = new IntersectionObserver
+//  function that adds the appear class and the appea options
  (function(entries, appearOnScroll) {
    entries.forEach(entry => {
     if(!entry.isIntersecting) {
@@ -56,10 +64,8 @@ window.addEventListener('scroll', () => {
    })
  }, appearOptions)
 
- faders.forEach(fader => {
-  appearOnScroll.observe(fader)
- })
 
+   //  observe only on the elements of the class fade-in
  cards.forEach(card => {
   appearOnScroll.observe(card)
  })
